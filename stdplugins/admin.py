a55fd25a436e59a520aa0288dc0627013abf7931
M.Furkan <byazade@yandex.com>
2020-01-16 11:29:43 +0300

    .

--- stdplugins/admin.py
+++ stdplugins/admin.py
@@ -423,8 +423,7 @@ async def muter(moot):
     """ Used for deleting the messages of muted people """
     if not is_mongo_alive():
         return
-    # muted = await get_muted(moot.chat_id)
-    muted = await get_muted(moot.from_id)
+    muted = await get_muted(moot.chat_id)
     gmuted = await get_gmuted()
     rights = ChatBannedRights(
         until_date=None,
