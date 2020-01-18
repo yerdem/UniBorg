fc73804a1e2b1caeefb12b38e63496a82b9f7214
M.Furkan <byazade@yandex.com>
2020-01-18 13:46:20 +0300

    ~~

--- stdplugins/antispam.py
+++ stdplugins/antispam.py
@@ -20,17 +20,17 @@ from uniborg.util import admin_cmd
 
 
 
-BANNED_RIGHTS = ChatBannedRights(
-    until_date=None,
-    view_messages=True,
-    send_messages=True,
-    send_media=True,
-    send_stickers=True,
-    send_gifs=True,
-    send_games=True,
-    send_inline=True,
-    embed_links=True,
-)
+# BANNED_RIGHTS = ChatBannedRights(
+#     until_date=None,
+#     view_messages=True,
+#     send_messages=True,
+#     send_media=True,
+#     send_stickers=True,
+#     send_gifs=True,
+#     send_games=True,
+#     send_inline=True,
+#     embed_links=True,
+# )
 
 
 @borg.on(events.ChatAction())
@@ -48,11 +48,17 @@ async def _(cas):
             if r_dict['ok']:
                 try:                
                     more = r_dict['result']
-                    # rights = ChatBannedRights(
-                    #     until_date=None,
-                    #     view_messages=True,
-                    #     send_messages=True
-                    # )
+                    rights = ChatBannedRights(
+                        until_date=None,
+                        view_messages=True,
+                        send_messages=True,
+                        send_media=True,
+                        send_stickers=True,
+                        send_gifs=True,
+                        send_games=True,
+                        send_inline=True,
+                        embed_links=True,
+                    )
                     # user_entity = cas.client.get_entity(PeerUser(id))
                     # print(user_entity)
                     await borg.edit_permissions(cas.chat_id, id, BANNED_RIGHTS)
