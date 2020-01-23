91c9b530634da2ac1bd5396808cc45c9e2932fa5
M.Furkan <byazade@yandex.com>
2020-01-23 18:51:48 +0300

    update

--- sample_config.py
+++ sample_config.py
@@ -61,10 +61,8 @@ class Config(object):
     NO_LOAD = [
         "notification_mtab_manager",
         "dbhelper",
-        "admin",
         "fban_gban",
-        "gban",
-        "antispambot"
+        "gban"
     ]
     # Get your own API key from https://www.remove.bg/ or
     # feel free to use http://telegram.dog/Remove_BGBot
