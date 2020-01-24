923bd00114e02362033e9ae47f84c0b099c2e980
M.Furkan <byazade@yandex.com>
2020-01-24 21:52:38 +0300

    .

--- stdplugins/account_profile.py
+++ stdplugins/account_profile.py
@@ -10,7 +10,7 @@ from uniborg.util import admin_cmd
 
 from sample_config import Config
 import logging
-from util import errors_handler
+
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
