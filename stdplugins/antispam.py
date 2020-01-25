ac2710e5e01c8bbb08153ee6e558862f29c87251
M.Furkan <byazade@yandex.com>
2020-01-25 11:02:47 +0300

    #fix

--- stdplugins/antispam.py
+++ stdplugins/antispam.py
@@ -47,7 +47,8 @@ async def _(cas):
                     how = "**How**: [here](https://combot.org/api/cas/check?user_id={})".format(id)
                     # moree = "**More**: ```{}```".format(more)
                     # entity = await borg.get_entity(chat)
-                    entity = borg.iter_participants(chat)
+                    async for g in client.iter_participants(chat):
+                        entity = g
                     await borg.edit_permissions(int('-100' + str(entity.id)), user.id, view_messages=False)
                     await borg.send_message(Config.PRIVATE_GROUP_BOT_API_ID, f"**antispam log** \n{who}\n{where}\n{how}\n**Action**: Banned",link_preview=False)
                 except (Exception) as exc:
