37c42726c10d425a52ccecabcb36b0e2c4314336
M.Furkan <byazade@yandex.com>
2020-01-27 17:42:47 +0300

    ..........

--- stdplugins/fastdownload.py
+++ stdplugins/fastdownload.py
@@ -95,3 +95,38 @@ async def _download(event):
 			print(str(e))
 			pass	
 	
+async def check_metadata(gid):
+	file = aria2.get_download(gid)
+	new_gid = file.followed_by_ids[0]
+	logger.info("Changing GID "+gid+" to "+new_gid)
+	return new_gid	
+
+async def progress_status(gid,event,previous):
+	try:
+		file = aria2.get_download(gid)
+		if not file.is_complete:
+			if not file.error_message:
+				msg = "Downloading File: `"+str(file.name) +"`\nSpeed: "+ str(file.download_speed_string())+"\nProgress: "+str(file.progress_string())+"\nTotal Size: "+str(file.total_length_string())+"\nStatus: "+str(file.status)+"\nETA:  "+str(file.eta_string())+"\n\n"
+				if previous != msg:
+					await event.edit(msg)
+					previous = msg
+			else:
+				logger.info(str(file.error_message))
+				await event.edit("Error : `{}`".format(str(file.error_message)))		
+				return
+			await asyncio.sleep(EDIT_SLEEP_TIME_OUT)	
+			await progress_status(gid,event,previous)
+		else:
+			await event.edit("File Downloaded Successfully: `{}`".format(file.name))
+			return
+	except Exception as e:
+		if " not found" in str(e) or "'file'" in str(e):
+			await event.edit("Download Canceled :\n`{}`".format(file.name))
+			return
+		elif " depth exceeded" in str(e):
+			file.remove(force=True)
+			await event.edit("Download Auto Canceled :\n`{}`\nYour Torrent/Link is Dead.".format(file.name))
+		else:
+			logger.info(str(e))
+			await event.edit("Error :\n`{}`".format(str(e)))
+			return			
