17815cdb0b599a0152e64559a19c76d02ccb93e8
M.Furkan <byazade@yandex.com>
2020-02-01 20:48:18 +0300

    :)

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -115,10 +115,12 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     if "\n" in x_reponse:
                         x_reponse, _ = x_reponse.split("\n")
                     response_json = json.loads(x_reponse)
-                    print(response_json)
+                    # print(response_json)
+                    # dump = json.dumps(response_json,"./DOWNLOADS/YouTubeDL.json",)
                     # save_ytdl_json_path = Config.TMP_DOWNLOAD_DIRECTORY + "YouTubeDL" + ".json"
-                    with open("./DOWNLOADS/YouTubeDL.json", "w", encoding="utf8") as outfile:
+                    with open("./DOWNLOADS/YouTubeDL.json", "w", encoding="utf-8") as outfile:
                         json.dump(response_json, outfile, ensure_ascii=False)
+                        # json.dump(response_json, outfile, ensure_ascii=False)    
                     # logger.info(response_json)
                     inline_keyboard = []
                     duration = None
