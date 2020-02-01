2a318f5e6bbb7544ca66d48428310d2ca18c7c28
M.Furkan <byazade@yandex.com>
2020-02-01 19:26:14 +0300

    .

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -114,8 +114,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     if "\n" in x_reponse:
                         x_reponse, _ = x_reponse.split("\n")
                     response_json = json.loads(x_reponse)
-                    save_ytdl_json_path = Config.TMP_DOWNLOAD_DIRECTORY + \
-                        "/" + "YouTubeDL" + ".json"
+                    save_ytdl_json_path = Config.TMP_DOWNLOAD_DIRECTORY + "/" + "YouTubeDL" + ".json"
                     with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
                         json.dump(response_json, outfile, ensure_ascii=False)
                     # logger.info(response_json)
@@ -263,7 +262,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {}".format("✅", x),
+        "{} {}".format("💎", x),
         data="ub_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
