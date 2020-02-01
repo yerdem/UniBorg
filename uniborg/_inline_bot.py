0126cfbaf84a2904924953e82e61d5d6956e6b47
M.Furkan <byazade@yandex.com>
2020-02-01 20:29:05 +0300

    shit that error

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -79,7 +79,7 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # input format should be ytdl URL
             p = re.compile("ytdl (.*)")
             r = p.search(query)
-            print(ytdl_url)
+            print(r.group(1))
             ytdl_url = r.group(1).strip()
             if ytdl_url.startswith("http"):
                 command_to_exec = [
