5d7a54f4d2427e815ee2a7784ca4b74eea903fc8
M.Furkan <byazade@yandex.com>
2020-02-01 19:31:38 +0300

    2

--- stdplugins/youtube_dl.py
+++ stdplugins/youtube_dl.py
@@ -4,9 +4,7 @@
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
 """YoutubeDL
 Click on any of the Buttons"""
-import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+
 import asyncio
 import json
 import os
@@ -24,14 +22,11 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == borg.uid:  # pylint:disable=E0602
             ctc, tg_send_type, ytdl_format_code, ytdl_extension = event.query.data.decode("UTF-8").split("|")
-            tg_send_type = event.query.data.decode("UTF-8").split("|")
-            ytdl_format_code = event.query.data.decode("UTF-8").split("|")
-            ytdl_extension = event.query.data.decode("UTF-8").split("|")
             try:
                 with open(Config.TMP_DOWNLOAD_DIRECTORY + "/" + "YouTubeDL.json", "r", encoding="utf8") as f:
                     response_json = json.load(f)
             except FileNotFoundError as e:
-                await event.edit(f"Something Bad Happened\n{e}")
+                await event.edit("Something Bad Happened")
                 return False
             custom_file_name = str(response_json.get("title")) + \
                 "_" + ytdl_format_code + "." + ytdl_extension
