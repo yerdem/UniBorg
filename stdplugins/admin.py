cc72742d3cfa61ed54d385965941566b9d69fc79
M.Furkan <byazade@yandex.com>
2020-02-03 13:35:28 +0300

    get deleted from group/channel latest message

--- stdplugins/admin.py
+++ stdplugins/admin.py
@@ -741,7 +741,7 @@ async def _(event):
     c = await event.get_chat()
     if c.admin_rights or c.creator:
         a = await borg.get_admin_log(event.chat_id,limit=1, edit=False, delete=True)
-        print(a.message)
+        print(a[0].old)
         for i in a:
           await event.edit(i.original.action.message)
     else:
