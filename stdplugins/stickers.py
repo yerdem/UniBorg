6cef9b084c651136ed3ee7a5d145ab58d301a9f2
M.Furkan <byazade@yandex.com>
2020-02-07 22:25:12 +0300

    new4

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -45,7 +45,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"@By_Azade Pack"
-    packshortname = f"@By_Azade"  # format: Uni_Borg_userid
+    packshortname = f"By_Azade"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@By_Azade.png"
@@ -55,7 +55,7 @@ async def _(event):
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"@By_Azade Pack"
-        packshortname = f"@By_Azade"  # format: Uni_Borg_userid
+        packshortname = f"By_Azade"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
