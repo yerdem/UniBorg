7c399ec5b91864994344687a4a7f49a315573da5
M.Furkan <byazade@yandex.com>
2020-02-08 12:57:45 +0300

    new plugin

--- stdplugins/ttf.py
+++ stdplugins/ttf.py
@@ -8,6 +8,6 @@ async def get(event):
     name = event.text[5:]
     m = await event.get_reply_message()
     with open(name, "w") as f:
-        f.write(m.message)
+        f.write(m.text)
     await event.delete()
     await borg.send_file(event.chat_id,name,force_document=True)
