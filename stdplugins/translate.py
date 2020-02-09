48ff5ac0a31abb652c2ecd4ed3a0dffe9e3e729d
M.Furkan <byazade@yandex.com>
2020-02-09 22:38:15 +0300

    .

--- stdplugins/translate.py
+++ stdplugins/translate.py
@@ -36,7 +36,7 @@ async def _(event):
         database = "./bin/language.json"
         data = json.loads(open(database).read())
         lang_detect = langid.classify(previous_message.message)
-        # print(lang_detect)
+        print(lang_detect)
         for a in range(len(data)):
             if lang_detect in data[a]["code"]:
                 full_lang = data[a]["name"]
