2e9912c7a95363cc095ddf550ee337b88009ebf7
M.Furkan <byazade@yandex.com>
2020-02-09 23:55:17 +0300

    fixed

--- stdplugins/translate.py
+++ stdplugins/translate.py
@@ -9,7 +9,8 @@ import emoji
 from googletrans import Translator
 from uniborg.util import admin_cmd
 import json
-
+import langdetect
+from textblob import TextBlob
 
 @borg.on(admin_cmd(pattern="tr ?(.*)"))
 async def _(event):
@@ -35,7 +36,7 @@ async def _(event):
         # either here, or before translation
         database = "./bin/language.json"
         data = json.loads(open(database).read())
-        lang_detect = detect_language(previous_message.message)
+        lang_detect = TextBlob(previous_message.message).detect_language()
         print(lang_detect)
         for a in range(len(data)):
             if lang_detect in data[a]["code"]:
@@ -52,18 +53,3 @@ async def _(event):
     except Exception as exc:
         await event.edit(str(exc))
 
-
-def detect_language(text):
-    # [START translate_detect_language]
-    """Detects the text's language."""
-    from google.cloud import translate_v2 as translate
-    translate_client = translate.Client()
-
-    # Text can also be a sequence of strings, in which case this method
-    # will return a sequence of results for each text.
-    result = translate_client.detect_language(text)
-
-    print('Text: {}'.format(text))
-    print('Confidence: {}'.format(result['confidence']))
-    print('Language: {}'.format(result['language']))
-    # [END translate_detect_language]
