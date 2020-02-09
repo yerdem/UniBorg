45034368cc31dce8912abcf010f9da992cb2fa4c
M.Furkan <byazade@yandex.com>
2020-02-09 12:37:44 +0300

    revert new plugin

--- stdplugins/translate.py
+++ stdplugins/translate.py
@@ -1,44 +1,44 @@
-# """ Google Google Translate
-# Available Commands:
-# .tr LanguageCode as reply to a message
-# .tr LangaugeCode | text to sepak"""
-# import logging
-# logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-#                     level=logging.WARNING)
-# import emoji
-# from googletrans import Translator
-# from uniborg.util import admin_cmd
+""" Google Google Translate
+Available Commands:
+.tr LanguageCode as reply to a message
+.tr LangaugeCode | text to sepak"""
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+import emoji
+from googletrans import Translator
+from uniborg.util import admin_cmd
 
 
-# @borg.on(admin_cmd(pattern="tr ?(.*)"))
-# async def _(event):
-#     if event.fwd_from:
-#         return
-#     input_str = event.pattern_match.group(1)
-#     if event.reply_to_msg_id:
-#         previous_message = await event.get_reply_message()
-#         text = previous_message.message
-#         lan = input_str or "ml"
-#     elif "|" in input_str:
-#         lan, text = input_str.split("|")
-#     else:
-#         await event.edit("`.tr LanguageCode` as reply to a message")
-#         return
-#     text = emoji.demojize(text.strip())
-#     lan = lan.strip()
-#     translator = Translator()
-#     try:
-#         translated = translator.translate(text, dest=lan)
-#         after_tr_text = translated.text
-#         # TODO: emojify the :
-#         # either here, or before translation
-#         output_str = """**Text:** {}\n**Detected Language:** {}\n\n**Translated to:**\n{}""".format(
-#             # after_tr_text,
-#             previous_message.message,
-#             lan,
-#             after_tr_text
-#             # translated.src
-#         )
-#         await event.edit(output_str)
-#     except Exception as exc:
-#         await event.edit(str(exc))
+@borg.on(admin_cmd(pattern="tr ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str or "ml"
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await event.edit("`.tr LanguageCode` as reply to a message")
+        return
+    text = emoji.demojize(text.strip())
+    lan = lan.strip()
+    translator = Translator()
+    try:
+        translated = translator.translate(text, dest=lan)
+        after_tr_text = translated.text
+        # TODO: emojify the :
+        # either here, or before translation
+        output_str = """**Text:** {}\n**Detected Language:** {}\n\n**Translated to:**\n{}""".format(
+            # after_tr_text,
+            previous_message.messa,
+            lan,
+            after_tr_text
+            # translated.src
+        )
+        await event.edit(output_str)
+    except Exception as exc:
+        await event.edit(str(exc))
--- stdplugins/translate_v2.py
+++ stdplugins/translate_v2.py
@@ -1,52 +1,52 @@
-import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
-import emoji
-from googletrans import Translator
-from uniborg.util import admin_cmd
-from mtranslate import translate
-from langdetect import detect
+# import logging
+# logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+#                     level=logging.WARNING)
+# import emoji
+# from googletrans import Translator
+# from uniborg.util import admin_cmd
+# from mtranslate import translate
+# from langdetect import detect
 
-@borg.on(admin_cmd(pattern="tr ?(.*)"))
-async def trtxxx(message):
-        """Translates desired text to whatever language.\n\n
-        Example usage:\n.trt <lang> with a replied message"""
-        target = (arg_split_with(message, " "))
-        if not target:
-            await message.edit("__Specify the target language.__")
-            return
-        if target and len(target) < 2 and not message.is_reply:
-            await message.edit("__Specify the text to be translated.__")
-            return
-        reply = await message.get_reply_message()
-        text = (
-            target[1] if not message.is_reply else
-            reply.text)
-        target = target[0]
-        if reply and not reply.text:
-            await message.edit("__Babe..Are you okay? You can not translate files you know.__")
-            return
-        await message.edit("__Translating...__")
-        result = translate(text, target, 'auto')
-        await message.edit(
-                           "**Text:** __{}__\n"
-                           "**Detected Language:** __{}__\n\n"
-                           "**Translated to:**\n__{}__"
-                           .format(text, detect(text), result))
+# @borg.on(admin_cmd(pattern="tr ?(.*)"))
+# async def trtxxx(message):
+#         """Translates desired text to whatever language.\n\n
+#         Example usage:\n.trt <lang> with a replied message"""
+#         target = (arg_split_with(message, " "))
+#         if not target:
+#             await message.edit("__Specify the target language.__")
+#             return
+#         if target and len(target) < 2 and not message.is_reply:
+#             await message.edit("__Specify the text to be translated.__")
+#             return
+#         reply = await message.get_reply_message()
+#         text = (
+#             target[1] if not message.is_reply else
+#             reply.text)
+#         target = target[0]
+#         if reply and not reply.text:
+#             await message.edit("__Babe..Are you okay? You can not translate files you know.__")
+#             return
+#         await message.edit("__Translating...__")
+#         result = translate(text, target, 'auto')
+#         await message.edit(
+#                            "**Text:** __{}__\n"
+#                            "**Detected Language:** __{}__\n\n"
+#                            "**Translated to:**\n__{}__"
+#                            .format(text, detect(text), result))
 
 
 
-def arg_split_with(message, char):
-    args = get_arg(message).split(char)
-    for space in args:
-        if space.strip() == "":
-            args.remove(space)
-    return args
+# def arg_split_with(message, char):
+#     args = get_arg(message).split(char)
+#     for space in args:
+#         if space.strip() == "":
+#             args.remove(space)
+#     return args
 
-def get_arg(message):
-    msg = message.raw_text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
+# def get_arg(message):
+#     msg = message.raw_text
+#     msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+#     split = msg[1:].replace("\n", " \n").split(" ")
+#     if " ".join(split[1:]).strip() == "":
+#         return ""
+#     return " ".join(split[1:])
