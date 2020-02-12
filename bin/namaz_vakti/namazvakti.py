4349fd2afd49aa9d5ec095db80d54177459e2c5d
M.Furkan <byazade@yandex.com>
2020-02-13 01:29:25 +0300

    .

--- bin/namaz_vakti/namazvakti.py
+++ bin/namaz_vakti/namazvakti.py
@@ -200,6 +200,8 @@ class namazvakti():
                 sonuc["durum"] = "basarili"
                 sonuc["veri"] = veri["veri"]
                 #cache belleğe ana işte burada yaz!
+                if not os.path.exists(cacheDosyasi):
+                    os.mknod(cacheDosyasi)
                 with open(cacheDosyasi, "w") as yaz:
                     json.dump(sonuc, yaz)
 
