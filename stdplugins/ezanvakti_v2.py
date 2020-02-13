8dfce03839e909a55c8f9e7d077a6f3b2fc548f3
M.Furkan <byazade@yandex.com>
2020-02-13 23:05:42 +0300

    .

--- stdplugins/ezanvakti_v2.py
+++ stdplugins/ezanvakti_v2.py
@@ -41,7 +41,7 @@ async def namaz_(event):
     else:
         LOKASYON_2 = event.pattern_match.group(2)
         if LOKASYON_2:
-            LOKASYON_2 = LOKASYON_2.replace('i','İ').upper
+            LOKASYON_2 = LOKASYON_2.replace('i','İ').upper()
     yer = './bin/namaz_vakti/db/yerler.ndb'
     with open(yer, "r", encoding="utf-8") as f:
         yerler_json = json.load(f)
