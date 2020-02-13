8888952073d0bebbd44b8df18a39f56667cd31ec
M.Furkan <byazade@yandex.com>
2020-02-13 14:29:45 +0300

    .

--- stdplugins/ezanvakti_v2.py
+++ stdplugins/ezanvakti_v2.py
@@ -47,7 +47,6 @@ async def namaz_(event):
     tz = pytz.timezone('Europe/Istanbul')
     istanbul_now = datetime.now(tz)
     bugun = istanbul_now.strftime("%d.%m.%Y")
-    bugun = int(bugun)
     print(sonuc['veri'])
     yer = sonuc['veri']['yer_adi']
     tarih = sonuc['veri']['vakit']['tarih'][bugun]['uzun_tarih']
