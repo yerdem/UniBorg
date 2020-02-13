0d951ff06de09c2994a000bf2825a402f58834fb
M.Furkan <byazade@yandex.com>
2020-02-13 13:58:12 +0300

    .

--- bin/namaz_vakti/namazvakti.py
+++ bin/namaz_vakti/namazvakti.py
@@ -199,7 +199,7 @@ class namazvakti():
                     error = None
                     pass
                 with open("./bin/namaz_vakti/db/cache/"+"cache_" + str(yer["sehir_id"]) + ".ndb", 'wt', encoding='utf-8') as ver:
-                    json.dumps(sonuc,ver)
+                    sonuc = json.dumps(ver)
                 # f.write(son_veri)
                 # json.dump(son_veri)
                 # f.close()
