98d5e26196b1d62159aa34488ba16d819d2a7b62
M.Furkan <byazade@yandex.com>
2020-02-16 11:22:41 +0300

    new rss plugin for uniborg

--- sample_config.py
+++ sample_config.py
@@ -64,8 +64,7 @@ class Config(object):
         "fban_gban",
         "gban",
         "unbanmute",
-        "antispam",
-        "rss"
+        "antispam"
     ]
     # Get your own API key from https://www.remove.bg/ or
     # feel free to use http://telegram.dog/Remove_BGBot
