8edefa80d4047192ac38fdea71b43baa3800c6c5
M.Furkan <byazade@yandex.com>
2020-02-17 11:14:36 +0300

    new

--- stdplugins/spamwatch.py
+++ stdplugins/spamwatch.py
@@ -39,31 +39,39 @@ BANNED_RIGHTS = ChatBannedRights(
 @borg.on(events.ChatAction())
 async def spam_watch_(event):
     # user = await get_user_from_event(event)
+    chat = await event.get_chat()
     client = spamwatch.Client(Config.SPAM_WATCH_API)
     # ban = client.get_ban(event.from_id)
     user = await event.get_user()
-    if event.user_joined or event.user_added:
-        try:
-            ban = client.get_ban(event.action_message.from_id)
-            if ban:
-                await borg(EditBannedRequest(event.chat_id,event.action_message.from_id,BANNED_RIGHTS))
-                await event.client.send_message(
-                LOGGING_CHATID,
-                "#SPAM_WATCH\n"
-                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
-                f"CHAT: {event.chat.title}(`{event.chat_id}`)"
-            )
-            else:
+    if (chat.admin_rights or chat.creator):
+        if event.user_joined or event.user_added:
+            try:
+                ban = client.get_ban(event.action_message.from_id)
+                if ban:
+                    await borg(EditBannedRequest(event.chat_id,event.action_message.from_id,BANNED_RIGHTS))
+                #     await event.client.send_message(
+                #     LOGGING_CHATID,
+                #     "#SPAM_WATCH\n"
+                #     f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                #     f"CHAT: {event.chat.title}(`{event.chat_id}`)"
+                # )
+                else:
+                    return
+            except AttributeError:
                 return
-        except BadRequestError:
-            return
-        if ENABLE_LOG:
-            await event.client.send_message(
-                LOGGING_CHATID,
-                "#SPAM_WATCH_BAN\n"
-                f"USER: [{user.first_name}](tg://user?id={user.id})\n"
-                f"CHAT: {event.chat.title}(`{event.chat_id}`)"
-            )
+            except BadRequestError:
+                return
+            if ENABLE_LOG:
+                await event.client.send_message(
+                    LOGGING_CHATID,
+                    "#SPAM_WATCH_BAN\n"
+                    f"USER: [{user.first_name}](tg://user?id={user.id})\n"
+                    f"CHAT: {event.chat.title}(`{event.chat_id}`)"
+                )
+        else:
+            return ""
+    else:
+        return ""
 
 
 # async def get_user_from_event(event):
