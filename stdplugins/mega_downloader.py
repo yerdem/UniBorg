751bf52412609d9601ebbead969a3c3ec1e6d8ca
M.Furkan <byazade@yandex.com>
2020-03-07 11:04:54 +0300

    edit

--- stdplugins/mega_downloader.py
+++ stdplugins/mega_downloader.py
@@ -8,7 +8,7 @@ import time
 from os.path import exists
 from subprocess import PIPE, Popen
 from urllib.error import HTTPError
-
+import logger
 from pySmartDL import SmartDL
 from uniborg.events import register
 from uniborg.util import admin_cmd, humanbytes
@@ -74,7 +74,7 @@ async def mega_download(url, megadl):
             downloader.start(blocking=False)
         except HTTPError as e:
             await megadl.edit("`" + str(e) + "`")
-            LOGS.info(str(e))
+            logger.info(str(e))
             return
         while not downloader.isFinished():
             status = downloader.get_status().capitalize()
@@ -101,7 +101,7 @@ async def mega_download(url, megadl):
                         await megadl.edit(current_message)
                         display_message = current_message
             except Exception as e:
-                LOGS.info(str(e))
+                logger.info(str(e))
         if downloader.isSuccessful():
             download_time = downloader.get_dl_time(human=True)
             if exists(temp_file_name):
@@ -113,7 +113,7 @@ async def mega_download(url, megadl):
         else:
             await megadl.edit("Failed to download...")
             for e in downloader.get_errors():
-                LOGS.info(str(e))
+                logger.info(str(e))
     return
 
 
